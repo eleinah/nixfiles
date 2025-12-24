bbdd1e1078e20f750fa7a1dcfec30da71af9246f
Eleina Hayden <eleina@member.fsf.org>
2025-12-24 17:57:41 -0600

    add some more git settings

--- users/ellie/home.nix
+++ users/ellie/home.nix
@@ -94,6 +94,13 @@
         user = {
           name = "Eleina Mironia";
           email = "eleina@member.fsf.org";
+          signingkey = "6339A81D6B416E97"
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        commit = {
+          gpgsign = true;
         };
       };
     };
