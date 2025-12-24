4fedba2f33a8be9bbab6fb3b9128c26d3e181390
Eleina Hayden <eleina@member.fsf.org>
2025-12-24 17:48:04 -0600

    oops i can't read

--- flake.nix
+++ flake.nix
@@ -1,14 +1,5 @@
 {
   description = "NixOS configuration";
-  system = "x86_64-linux";
-  hm-ellie = {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-
-        home-manager.users = {
-          ellie = import ./users/ellie.nix;
-        };
-      };
 
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
@@ -18,35 +9,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      nix-lab = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./common.nix
-          ./hosts/lab/hardware-configuration.nix
-          ./hosts/lab/configuration.nix
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      hm-ellie = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
 
-          home-manager.nixosModules.home-manager hm-ellie
-        ];
+        home-manager.users = {
+          ellie = import ./users/ellie.nix
+        };
       };
-      mainstation = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./common.nix
-          ./hosts/mainstation/hardware-configuration.nix
-          ./hosts/mainstation/configuration.nix
+    in {
+      nixosConfigurations = {
+        nix-lab = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./common.nix
+            ./hosts/lab/hardware-configuration.nix
+            ./hosts/lab/configuration.nix
 
-          home-manager.nixosModules.home-manager hm-ellie
-        ];
-      };
-      travelstation = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./common.nix
-          ./hosts/travelstation/hardware-configuration.nix
-          ./hosts/travelstation/configuration.nix
+            home-manager.nixosModules.home-manager hm-ellie
+          ];
+        };
+        mainstation = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./common.nix
+            ./hosts/mainstation/hardware-configuration.nix
+            ./hosts/mainstation/configuration.nix
 
-          home-manager.nixosModules.home-manager hm-ellie
-        ];
+            home-manager.nixosModules.home-manager hm-ellie
+          ];
+        };
+        travelstation = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./common.nix
+            ./hosts/travelstation/hardware-configuration.nix
+            ./hosts/travelstation/configuration.nix
+
+            home-manager.nixosModules.home-manager hm-ellie
+          ];
+        };
       };
-    };
   };
 }
