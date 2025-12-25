cd93a1af9d0e33a53311709006c84f203dcf0888
Eleina Hayden <eleina@member.fsf.org>
2025-12-24 21:41:18 -0600

    move scripts up a couple levels

--- users/ellie/home.nix
+++ users/ellie/home.nix
@@ -9,6 +9,10 @@
       ".config" = {
         source = ./dots/.config;
         recursive = true;
+      };
+      "scripts" = {
+        source = ./scripts;
+        recursive = true;
         executable = true;
       };
     };
