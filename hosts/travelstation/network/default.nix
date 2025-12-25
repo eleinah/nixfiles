b90aee41fe067a013c05a791808ebf85cf5355ae
Eleina Hayden <eleina@member.fsf.org>
2025-12-25 14:19:47 -0600

    remove profile, add wireguard vpn

--- hosts/travelstation/network/default.nix
+++ hosts/travelstation/network/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./sys.nix
+    ./wireguard.nix
   ];
 }
---
+++ hosts/travelstation/network/wireguard.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
+  ];
+
+  networking.wireguard.interfaces {
+    wg0 = {
+      ips = [ "10.99.99.99/32" ];
+
+      # TODO: get secrets manager working
+      privateKeyFile = "";
+
+      peers = [
+        {
+          publicKey = "/yHUcOEdNd9L/CDlKu9zw/xU4O4ENLK2iguE/MH/20M=";
+          endpoint = "vpn.eleina.sh:51820";
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+          persistentKeepalive = 25;
+        };
+      ];
+    };
+  };
+}
