14abe27f3db6a4471a5697c5b326a228886d563b
Eleina Mironia <eleina@member.fsf.org>
2025-12-26 02:15:00 -0600

    add more langs, for doom emacs

--- users/ellie/packages/langs.nix
+++ users/ellie/packages/langs.nix
@@ -4,5 +4,20 @@
   home.packages = with pkgs; [
     d2
     shellcheck
+    shfmt
+    rustup
+    python
+    python-black
+    python-pyflakes
+    python-isort
+    python-pipenv
+    python-nosetests
+    python-pytest
+    nix
+    golang
+    xmllint
+    npm
+    cmake
+    make
   ];
 }
