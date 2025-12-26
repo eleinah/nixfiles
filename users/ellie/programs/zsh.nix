8a098775035ef228cd22a7e90091450b10463d5f
Eleina Mironia <eleina@member.fsf.org>
2025-12-26 04:31:48 -0600

    Update zsh.nix

--- users/ellie/programs/zsh.nix
+++ users/ellie/programs/zsh.nix
@@ -6,7 +6,7 @@
     enableCompletion = true;
     loginExtra = ''
       if [[ -z "$WAYLAND_DISPLAY" && -n "$XDG_VTNR" && "$XDG_VTNR" -eq 1 ]]; then
-          exec niri-session
+          exec niri-session -l
       fi
     '';
     profileExtra = ''
