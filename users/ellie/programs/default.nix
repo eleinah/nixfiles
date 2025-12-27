1eefae3d1f1ddb27217ea5d407d975b34e736670
Eleina Mironia <eleina@member.fsf.org>
2025-12-27 16:22:58 -0600

    rid niriswitcher

--- users/ellie/programs/default.nix
+++ users/ellie/programs/default.nix
@@ -7,7 +7,6 @@
     ./firefox.nix
     ./git.nix
     ./kitty.nix
-    ./niriswitcher.nix
     ./starship.nix
     ./zsh.nix
   ];
--- users/ellie/programs/niriswitcher.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.niriswitcher = {
-    enable = true;
-  };
-}
