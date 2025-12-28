d1162303cd7265872abc8a477d0ae2de38cf8aec
Eleina Mironia <eleina@member.fsf.org>
2025-12-28 14:49:58 -0600

    add networkmanager profile

---
+++ servers/boxoffice/network/networkmanager.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  networking.networkmanager = {
+
+    ensureProfiles.profiles = {
+      "Wired connection 1" = {
+        connection = {
+          id = "Wired connection 1";
+          type = "802-3-ethernet";
+        };
+        ipv4 = {
+          method = "auto";
+          dns = "10.4.10.5";
+          dns-search = "lly.local";
+        };
+      };
+    };
+
+  };
+}
