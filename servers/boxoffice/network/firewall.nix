01e8be817fde17679f6417889d3400fea6607ecb
Eleina Mironia <eleina@member.fsf.org>
2025-12-28 18:22:54 -0600

    open another port for qbittorrent

--- servers/boxoffice/network/firewall.nix
+++ servers/boxoffice/network/firewall.nix
@@ -5,7 +5,8 @@
     allowedTCPPorts = [
       8096 # Jellyfin
       8097 # Jellyseerr
-      8080 # qBittorrent
+      8080 # qBittorrent web ui
+      27421 # qBittorrent listen
       7878 # Radarr
       8989 # Sonarr
       9696 # Prowlarr
