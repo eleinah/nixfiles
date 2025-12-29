0d0396cc41b99a9ac43958d9c227d313fae1a8b5
Eleina Mironia <eleina@member.fsf.org>
2025-12-28 19:37:49 -0600

    add individual .gnupg files instead

--- users/ellie/home/default.nix
+++ users/ellie/home/default.nix
@@ -14,6 +14,18 @@
       executable = true;
     };
 
+    ".gnupg/gpg-agent.conf" = {
+      source = ./dots/.gnupg/gpg-agent.conf;
+    };
+
+    ".gnupg/gpg.conf" = {
+      source = ./dots/.gnupg/gpg.conf;
+    };
+
+    ".gnupg/scdaemon.conf" = {
+      source = ./dots/.gnupg/scdaemon.conf;
+    };
+
     ".ssh/config" = {
       source = ./dots/.ssh/config;
     };
